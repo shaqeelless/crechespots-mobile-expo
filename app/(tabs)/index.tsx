783b6720afa1e889972a01e584979e058ab096ca
Shaqeel less <50143459+shaqeelless@users.noreply.github.com>
2025-11-17 09:05:32 +0000

    update sliding color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1090,7 +1090,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingBottom: 24,
     borderEndEndRadius: 150,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#bd84f6',
   },
   headerTop: {
     flexDirection: 'row',
